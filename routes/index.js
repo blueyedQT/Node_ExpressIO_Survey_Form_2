df4c5cfaaaa9e88a3caac5ac8332e105185d3760
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-07 16:50:53 -0800

    Working listen and emitt between client and server

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,14 @@ module.exports = function Route(app){
 	app.get('/', function(req, res){
 		res.render('index', {title: 'Survey Form'});
 	});
-	app.post('/result', function(req, res){
-		var user = {};
-		user.name = req.body.name;
-		user.location = req.body.location;
-		user.language = req.body.language;
-		user.comment = req.body.comment;
-		user.title = 'Survey Form';
-		res.render('result', user);
-	});
-	app.get('/result', function(req, res){
-		res.render('result', {title: 'Survey Form'});
+	app.io.route('posting_form', function(req) {
+		console.log(req.data.value);
+		var message = 'You emitted the following information to the server: </br>'+
+					  'Name: '+req.data.name+'</br>'+
+					  'Location: '+req.data.location+'</br>'+
+					  'Language: '+req.data.language+'</br>'+
+					  'Comments: '+req.data.comment;
+		console.log(message);
+		req.io.emit('updated_message', message);
 	});
 }
