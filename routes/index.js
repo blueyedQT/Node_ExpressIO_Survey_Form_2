1c2e4b0973d71529ecaf60db1cba32c9641ccb2f
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-07 17:04:23 -0800

    Lucky number (random) feature added

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ module.exports = function Route(app){
 		res.render('index', {title: 'Survey Form'});
 	});
 	app.io.route('posting_form', function(req) {
-		console.log(req.data.value);
 		var message = 'You emitted the following information to the server: </br>'+
 					  'Name: '+req.data.name+'</br>'+
 					  'Location: '+req.data.location+'</br>'+
 					  'Language: '+req.data.language+'</br>'+
-					  'Comments: '+req.data.comment;
-		console.log(message);
+					  'Comments: '+req.data.comment+'</br></br>';
 		req.io.emit('updated_message', message);
+		var rand = Math.floor(Math.random()*1000)+1;
+		var rand_message = 'Your lucky number emitted by the server is: '+rand;
+		req.io.emit('random_number', rand_message);
 	});
 }
